50e8443f8f6e0ffdc098b7838ff0808712ebe2b6
Pavel <s4idm4de@yandex.ru>
2023-05-06 18:54:53 +0300

    aaaa

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDate;
 
 @Data
